edfcc089effea789f89a1b41d463dea955b3cfa0
Carlos Umaña Acevedo <carumtito@gmail.com>
2023-04-09 13:55:35 -0600

    Player animation

--- src/entities/entity.js
+++ src/entities/entity.js
@@ -1,36 +1,56 @@
 import { GameElement } from '../core/game-element.js'
 
 export class Entity extends GameElement {
-    constructor({ x, y, width, height }) {
+    constructor({ x, y, width, height, src }) {
         super({ x, y, width, height })
 
-        this.img = new Image()
-		this.img.src = './assets/img/player.png'
+        this.image = new Image()
+		this.image.src = src
+
+        this.frames = {
+            current: 0,
+            elapsed: 0,
+            max: 0,
+            speed: 0,
+        }
     }
 
-    draw(ctx) {
-        // Position on the sprite
-        const positionSprite = {
-            x: 0,
-            y: 0,
+    update() {
+        this.frames.elapsed = (this.frames.elapsed + 1) % 60
+
+        if (this.frames.max > 1 && this.frames.elapsed % this.frames.speed === 0) {
+            this.frames.current = (this.frames.current + 1) % this.frames.max
+        }
+    }
+
+    draw(ctx, sprite, flip = false) {
+        const { image, x: dx, y: dy, width: dw, height: dh } = this
+		const { x: sx, y: sy, w: sw, h: sh, offsetX, offsetY } = sprite
+
+        const draw = () => ctx.drawImage(
+            image,
+            sx + (sw * this.frames.current),
+            sy,
+
+            sw,
+            sh,
+
+            flip ? (-dx - dw - offsetX) : (dx - offsetX),
+            dy - offsetY,
+
+            sw,
+            sh,
+        )
+
+        if (flip) {
+            ctx.save()
+            ctx.scale(-1, 1)
+            draw()
+            ctx.restore()
+        } else {
+            draw()
         }
 
-        // Position on the canvas
-        const positionCanvas = {
-            x: this.x - 18,
-            y: this.y - 18,
-        }
-
-		ctx.drawImage(
-			this.img,
-			positionSprite.x,
-			positionSprite.y,
-			48,
-			this.img.height,
-			positionCanvas.x,
-			positionCanvas.y,
-			48,
-			this.img.height,
-		)
+        // super.draw(ctx)
 	}
 }
--- src/player.js
+++ src/player.js
@@ -1,15 +1,71 @@
 import { Entity } from './entities/entity.js'
 
-const STATUS = {
-	idle: 'idle',
-	running: 'running',
-	jumping: 'jumping',
-}
+const sprite = {
+    idle: {
+        x: 0,
+        y: 0,
+        w: 48,
+        h: 32,
 
-const SPRITES = {
-	[STATUS.idle]: {},
-	[STATUS.running]: {},
-	[STATUS.jumping]: {},
+        offsetX: 10,
+        offsetY: 7,
+
+        frames: 10,
+
+        speed: 5,
+    },
+    jump: {
+        x: 0,
+        y: 64,
+        w: 48,
+        h: 32,
+
+        offsetX: 10,
+        offsetY: 7,
+
+        frames: 1,
+
+        speed: 0,
+    },
+    fall: {
+        x: 48,
+        y: 64,
+        w: 48,
+        h: 32,
+
+        offsetX: 10,
+        offsetY: 7,
+
+        frames: 1,
+
+        speed: 18,
+    },
+    falling: {
+        x: 96,
+        y: 64,
+        w: 48,
+        h: 32,
+
+        offsetX: 10,
+        offsetY: 7,
+
+        frames: 1,
+
+        speed: 0,
+    },
+    run: {
+        x: 0,
+        y: 32,
+        w: 48,
+        h: 32,
+
+        offsetX: 10,
+        offsetY: 7,
+
+        frames: 8,
+
+        speed: 5,
+    },
 }
 
 export class Player extends Entity {
@@ -18,31 +74,41 @@ export class Player extends Entity {
 			x: 0,
 			y: 0,
 			width: 25,
-			height: 25,
+			height: 26,
+			src: './assets/img/player_king.png',
 		})
 
 		this.vx = 0
 		this.vy = 0
 
-		this.gravity = 1.8
+		this.gravity = 0.5
 		this.velocity = 4
 
-		this.status = STATUS.idle
+		this.status = 'idle'
+
+		this.lastMovement = null
+
+		this.direction = 'right'
 	}
 
 	movements(controls) {
 		const { direction } = controls
 
+		if (JSON.stringify(direction) === JSON.stringify(this.lastMovement)) return
+		this.lastMovement = { ...direction }
+
 		this.vx = 0
 
 		if (direction.left) {
 			this.vx = -this.velocity
+			this.direction = 'left'
 		} else if (direction.right) {
 			this.vx = this.velocity
+			this.direction = 'right'
 		}
 
 		if (direction.up && this.vy === 0) {
-			this.vy = -20
+			this.vy = -10
 		}
 	}
 
@@ -50,6 +116,41 @@ export class Player extends Entity {
 		this.vy += this.gravity
 		this.x += this.vx
 		this.y += this.vy
+
+		super.update()
+	}
+
+	draw(ctx) {
+		const isRunning = this.vx !== 0
+		const isJumping = this.vy < 0
+		const isFalling = this.vy > 0
+		const isFall = this.status === 'falling' && this.vy === 0
+		
+		let status = ''
+
+		if (isRunning) {
+			status = 'run'
+		} else if (isJumping) {
+			status = 'jump'
+		} else if (isFalling) {
+			status = 'falling'
+		} else if (isFall) {
+			status = 'fall'
+		} else {
+			status = 'idle'
+		}
+
+		if (this.status !== status) {
+			this.status = status
+
+			this.frame = 0
+			this.frames.current = 0
+        	this.frames.elapsed = 0
+        	this.frames.max = sprite[this.status].frames
+        	this.frames.speed = sprite[this.status].speed
+		}
+
+		super.draw(ctx, sprite[this.status], this.direction === 'left')
 	}
 
 	collision(blocks) {
