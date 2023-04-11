6aa5fb7d600f0c80fb113d6f3279d7df6b9234b0
Carlos Umaña Acevedo <carumtito@gmail.com>
2023-04-10 18:16:38 -0600

    Move sprites to json file

--- src/king-pig.js
+++ src/king-pig.js
@@ -1,5 +1,7 @@
 import { Enemy } from './entities/enemy.js'
-import { Sprite, SpritesNames } from './core/sprite.js'
+import { Sprite } from './core/sprite.js'
+
+import Sprites from '../assets/sprites/king-pig.json' assert { type: 'json' }
 
 export class KingPig extends Enemy {
     constructor({ x, y }) {
@@ -10,125 +12,7 @@ export class KingPig extends Enemy {
 			height: 26,
 			sprite: new Sprite({
                 src: './assets/img/pig_king.png',
-                sprites: {
-                    [SpritesNames.IDLE]: {
-                        x: 0,
-                        y: 0,
-                        w: 32,
-                        h: 32,
-    
-                        offsetX: 7,
-                        offsetY: 6,
-    
-                        frames: 12,
-    
-                        speed: 5,
-                    },
-                    [SpritesNames.JUMP]: {
-                        x: 0,
-                        y: 64,
-                        w: 32,
-                        h: 32,
-    
-                        offsetX: 7,
-                        offsetY: 6,
-    
-                        frames: 1,
-    
-                        speed: 0,
-                    },
-                    [SpritesNames.GROUND]: {
-                        x: 32,
-                        y: 64,
-                        w: 32,
-                        h: 32,
-    
-                        offsetX: 7,
-                        offsetY: 6,
-    
-                        frames: 1,
-    
-                        speed: 350,
-                    },
-                    [SpritesNames.FALL]: {
-                        x: 64,
-                        y: 64,
-                        w: 32,
-                        h: 32,
-    
-                        offsetX: 7,
-                        offsetY: 6,
-    
-                        frames: 1,
-    
-                        speed: 0,
-                    },
-                    [SpritesNames.RUN]: {
-                        x: 0,
-                        y: 32,
-                        w: 32,
-                        h: 32,
-    
-                        offsetX: 7,
-                        offsetY: 6,
-    
-                        frames: 6,
-    
-                        speed: 5,
-                    },
-                    [SpritesNames.ATTACK]: {
-                        x: 0,
-                        y: 128,
-                        w: 32,
-                        h: 32,
-    
-                        offsetX: 10,
-                        offsetY: 20,
-    
-                        frames: 5,
-
-                        speed: 5,
-                    },
-                    [SpritesNames.DIE]: {
-                        x: 0,
-                        y: 96,
-                        w: 32,
-                        h: 32,
-    
-                        offsetX: 7,
-                        offsetY: 6,
-    
-                        frames: 4,
-    
-                        speed: 15,
-                    },
-                    [SpritesNames.DEAD]: {
-                        x: 96,
-                        y: 96,
-                        w: 32,
-                        h: 32,
-                
-                        offsetX: 7,
-                        offsetY: 6,
-                
-                        frames: 1,
-                
-                        speed: 0,
-                    },
-                    [SpritesNames.HURT]: {
-                        x: 160,
-                        y: 128,
-                        w: 32,
-                        h: 32,
-    
-                        offsetX: 7,
-                        offsetY: 6,
-
-                        frames: 2,
-    
-                        speed: 8,
-                    },
-                }
+                sprites: Sprites
             })
         })
     }
--- src/king.js
+++ src/king.js
@@ -1,5 +1,7 @@
 import { Player } from './entities/player.js'
-import { Sprite, SpritesNames } from './core/sprite.js'
+import { Sprite } from './core/sprite.js'
+
+import Sprites from '../assets/sprites/king.json' assert { type: 'json' }
 
 export class King extends Player {
     constructor() {
@@ -10,138 +12,7 @@ export class King extends Player {
 			height: 26,
 			sprite: new Sprite({
                 src: './assets/img/player_king.png',
-                sprites: {
-                    [SpritesNames.IDLE]: {
-                        x: 0,
-                        y: 0,
-                        w: 48,
-                        h: 32,
-    
-                        offsetX: 10,
-                        offsetY: 6,
-    
-                        frames: 10,
-    
-                        speed: 5,
-                    },
-                    [SpritesNames.JUMP]: {
-                        x: 0,
-                        y: 64,
-                        w: 48,
-                        h: 32,
-    
-                        offsetX: 10,
-                        offsetY: 6,
-    
-                        frames: 1,
-    
-                        speed: 0,
-                    },
-                    [SpritesNames.GROUND]: {
-                        x: 48,
-                        y: 64,
-                        w: 48,
-                        h: 32,
-    
-                        offsetX: 10,
-                        offsetY: 6,
-    
-                        frames: 1,
-    
-                        speed: 350,
-                    },
-                    [SpritesNames.FALL]: {
-                        x: 96,
-                        y: 64,
-                        w: 48,
-                        h: 32,
-    
-                        offsetX: 10,
-                        offsetY: 6,
-    
-                        frames: 1,
-    
-                        speed: 0,
-                    },
-                    [SpritesNames.RUN]: {
-                        x: 0,
-                        y: 32,
-                        w: 48,
-                        h: 32,
-    
-                        offsetX: 10,
-                        offsetY: 6,
-    
-                        frames: 8,
-    
-                        speed: 5,
-                    },
-                    [SpritesNames.ATTACK]: {
-                        x: 0,
-                        y: 224,
-                        w: 80,
-                        h: 65,
-    
-                        offsetX: 10,
-                        offsetY: 20,
-    
-                        frames: 3,
-
-                        speed: 8,
-                    },
-                    [SpritesNames.DIE]: {
-                        x: 0,
-                        y: 96,
-                        w: 48,
-                        h: 32,
-    
-                        offsetX: 10,
-                        offsetY: 6,
-    
-                        frames: 4,
-    
-                        speed: 15,
-                    },
-                    [SpritesNames.DEAD]: {
-                        x: 144,
-                        y: 96,
-                        w: 48,
-                        h: 32,
-                
-                        offsetX: 10,
-                        offsetY: 6,
-                
-                        frames: 1,
-                
-                        speed: 0,
-                    },
-                    [SpritesNames.HURT]: {
-                        x: 240,
-                        y: 240,
-                        w: 48,
-                        h: 32,
-    
-                        offsetX: 10,
-                        offsetY: 6,
-    
-                        frames: 2,
-    
-                        speed: 8,
-                    },
-                    door_in: {
-                        x: 0,
-                        y: 176,
-                        w: 48,
-                        h: 48,
-                
-                        offsetX: 10,
-                        offsetY: 25,
-                
-                        frames: 8,
-                
-                        speed: 8,
-                    }
-                }
+                sprites: Sprites
             })
         })
     }
